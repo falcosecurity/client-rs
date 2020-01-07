26d27e6e182e79091092088748c8be741e62bb55
Leonardo Di Donato <leodidonato@gmail.com>
2020-01-07 16:48:48 +0100

    new(src): bootstramp credentials
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use futures::*;
 
-use grpcio::{ChannelBuilder, EnvBuilder};
+use grpcio::{ChannelBuilder, ChannelCredentialsBuilder, EnvBuilder, ChannelCredentials};
 use log;
 use protos::output;
 use protos::output_grpc;
@@ -8,7 +8,13 @@ use std::sync::Arc;
 
 fn main() {
     let env = Arc::new(EnvBuilder::new().build());
-    let ch = ChannelBuilder::new(env).connect("localhost:5060");
+    let mut cb = ChannelCredentialsBuilder::new();
+    // Set the PEM encoded server root cert to verify server's identity
+    cb.root_cert();
+    // Set the PEM encoded client side cert and key
+    cb.cert();
+    let cc = cb.build();
+    let ch = ChannelBuilder::new(env).secure_connect("localhost:5060", cc);
     let sc = output_grpc::ServiceClient::new(ch);
     let mut req = output::request::default();
     req.keepalive = true;
