0398d9559dd2a30fc0cb609b867b75d2358707d6
Leonardo Di Donato <leodidonato@gmail.com>
2020-01-08 15:49:25 +0000

    docs: client docs
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
+//! This crate provides a clean, ready to use client for [Falco](https://github.com/falcosecurity/falco), a
+//! Cloud Native Runtime Security tool written in C++.
+//!
+//! With this crate you can easily connect to the Falco gRPC API, interact with it, receive the alerts, and ...
+//!
+
 pub mod client;
 mod protos;
