a22075850d41c0a0bc5e774093321390cb6cda62
Leonardo Di Donato <leodidonato@gmail.com>
2020-01-09 12:49:34 +0000

    fix(src): result requires a 2nd type (error)
    Plus other fixes - ie., return type of Connect trait
    
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- src/client/client.rs
+++ src/client/client.rs
@@ -1,27 +1,30 @@
 use std::sync::Arc;
 
+/// A result holding an [`Error`](Error).
+// pub type Result<T> = result::Result<T, dyn Error>;
+
 use crate::{
     config,
-    protos::{output, output_grpc},
+    // protos::{output, output_grpc},
 };
-use futures::*;
+// use futures::*;
 use grpcio::{ChannelBuilder, ChannelCredentialsBuilder, Environment};
 use std::{fs::File, io::Read, path::Path};
 
 pub trait Connect {
-    fn connect(&self);
+    fn connect(&self, config: config::Config) -> Arc<grpcio::Channel>;
 }
 
 pub struct FalcoConnect {
     pub env: Arc<Environment>,
 }
 
-fn check_pem_file(path: &Path) -> Result<File> {
+fn check_pem_file(path: &Path) -> Result<File, std::io::Error> {
     File::open(path)
     // .map_err(|e| internal_err!("failed to open {} to load {}: {:?}", path.display(), tag, e))
 }
 
-fn load_pem_file(path: &Path) -> Result<Vec<u8>> {
+fn load_pem_file(path: &Path) -> Result<Vec<u8>, std::io::Error> {
     let mut file = check_pem_file(path)?;
     let mut key = vec![];
     file.read_to_end(&mut key)
@@ -40,21 +43,20 @@ impl Connect for FalcoConnect {
     fn connect(&self, config: config::Config) -> Arc<grpcio::Channel> {
         let credentials = ChannelCredentialsBuilder::new()
             // Set the PEM encoded server root cert to verify server's identity
-            .root_cert(load_pem_file(config.ca.unwrap().as_ref()))
+            .root_cert(load_pem_file(config.ca.unwrap().as_ref()).unwrap())
             // Set the PEM encoded client side cert and key
             .cert(
-                load_pem_file(config.cert.unwrap().as_ref()),
-                load_pem_file(config.key.unwrap().as_ref()),
+                load_pem_file(config.cert.unwrap().as_ref()).unwrap(),
+                load_pem_file(config.key.unwrap().as_ref()).unwrap(),
             )
             // Create channel credentials
             .build();
         let ch = ChannelBuilder::new(self.env.clone())
             .secure_connect(config.endpoint.as_str(), credentials);
         Arc::new(ch)
-
-        //TODO(fntlnz,leodido): keepalive, timeout, reconnect ?
     }
 }
+//TODO(fntlnz,leodido): keepalive, timeout, reconnect ?
 
 #[derive(Clone)]
 pub struct Client {
@@ -62,12 +64,12 @@ pub struct Client {
 }
 
 impl Client {
-    pub fn new(config: config::Config) -> Result<Client> {
+    pub fn new(config: config::Config) -> Result<Client, ()> {
         // let ca_crt = include_str!("/tmp/certs/ca.crt").into();
         // let client_crt = include_str!("/tmp/certs/client.crt").into();
         // let client_key = include_str!("/tmp/certs/client.key").into();
 
-        Ok(Client {})
+        Ok(Client{})
     }
 
     // pub fn outputs(&self, key: impl Into<Key>) -> impl Future<Output = Result<Option<Value>>> {
