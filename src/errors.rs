ee16ae8c36b35c29d000ff0df9d70945d7bbab8b
Lorenzo Fontana <lo@linux.com>
2020-01-13 17:08:24 +0100

    new: errors type definition
    Co-Authored-By: Leonardo Di Donato <leodidonato@gmail.com>
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

---
+++ src/errors.rs
@@ -0,0 +1,63 @@
+use failure::{Backtrace, Context, Fail};
+use std::fmt::{self, Display};
+use std::result;
+
+#[derive(Debug)]
+pub struct Error {
+    inner: Context<ErrorKind>,
+}
+
+impl Error {
+    pub fn kind(&self) -> &ErrorKind {
+        self.inner.get_context()
+    }
+}
+
+#[derive(Debug, Fail)]
+pub enum ErrorKind {
+    /// Wraps a `std::io::Error`.
+    #[fail(display = "IO error: {}", _0)]
+    Io(#[fail(cause)] std::io::Error),
+    /// Wraps a `grpcio::Error`.
+    #[fail(display = "gRPC error: {}", _0)]
+    Grpc(#[fail(cause)] grpcio::Error),
+}
+
+impl Fail for Error {
+    fn cause(&self) -> Option<&dyn Fail> {
+        self.inner.cause()
+    }
+
+    fn backtrace(&self) -> Option<&Backtrace> {
+        self.inner.backtrace()
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        Display::fmt(&self.inner, f)
+    }
+}
+
+impl From<ErrorKind> for Error {
+    fn from(kind: ErrorKind) -> Error {
+        Error {
+            inner: Context::new(kind),
+        }
+    }
+}
+
+impl From<grpcio::Error> for Error {
+    fn from(err: grpcio::Error) -> Self {
+        Error::from(ErrorKind::Grpc(err))
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Error::from(ErrorKind::Io(err))
+    }
+}
+
+/// Result with an error
+pub type Result<T> = result::Result<T, Error>;
